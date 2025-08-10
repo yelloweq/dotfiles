c7c24544e81482fd0471ab7b877a38e8b8f6fcc0
yelloweq <iwo.grzes237@gmail.com>
2025-08-10 23:00:03 +0100

    change to adibhanna settings for laravel dev stuff
    + change lsp to intelephense
    + remove queries
    + modify some keybinds

--- config/nvim/lua/custom/plugins/blade-nav.lua
+++
@@ -1,9 +0,0 @@
--- Add the blade-nav.nvim plugin which provides Goto File capabilities
--- for Blade files.
-return {
-  'ricardoramirezr/blade-nav.nvim',
-  dependencies = {
-    'hrsh7th/nvim-cmp',
-  },
-  ft = { 'blade', 'php' },
-}
--- config/nvim/lua/custom/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'github/copilot.vim',
-  event = 'InsertEnter',
-  config = function()
-    -- Disable the default mapping for Tab
-    vim.g.copilot_no_tab_map = true
-    vim.keymap.set('i', '<C-e>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
-  end,
-}
